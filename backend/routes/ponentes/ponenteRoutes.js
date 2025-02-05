e14379982902bb386fdd25cd8fa98c549b2e38c1
Manuel Guayasamin <manuelalejandro013@gmail.com>
2025-02-05 18:26:41 -0500

    actualizar ponente

--- backend/routes/ponentes/ponenteRoutes.js
+++ backend/routes/ponentes/ponenteRoutes.js
@@ -5,5 +5,7 @@ const ponenteController = require("../../controllers/ponenteController");
 // Rutas CRUD para ponentes
 router.post("/", ponenteController.createPonente);
 router.get("/", ponenteController.getAllPonentes);
+router.put("/:id", ponenteController.updatePonente);
+router.delete("/:id", ponenteController.deletePonente);
 
 module.exports = router;
